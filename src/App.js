a4db3814b5fa7c8ccd77e8fb2ebf24299fe36cd6
Ariady Putra <ariady.putra@gmail.com>
2022-10-19 00:15:53 +0700

    Code splitting

--- src/App.js
+++ src/App.js
@@ -7,24 +7,43 @@ import logo from './Nami_ADA.png';
 import './App.css';
 
 function App() {
+  const addr = 'addr1qyh99v0nhc8e7vcvl3gy8lhwkg7h3ykqgy2nud6gdkdad3fj9q6vf6cgnw48r2ljtmaauxn4s44uuskz3hvggjsslkaqd0rgze';
+  
+  const [wallets, setWallets] = React.useState(() => [{}]);
+  const [ada, setAda] = React.useState(() => 1);
+  
   const [txHash, setTxHash] = React.useState(() => {});
   const [error, setError] = React.useState(() => {});
   
   return (
     <div className="App">
-      <h1>Testnet Preview</h1>
-      <table><tr><td>
-        <img src={logo} className='Item' alt='Item 1'/>
-      </td></tr><tr><td>
-        Give me: 5 tADA
-      </td></tr><tr><td>
-        
-        <PurchaseButton label={'Send using'} callback={setTxHash} onError={setError} txInfo={{
-          toAddress : 'addr_test1qp75v9ld0084zl07kwjyfnwagm7nsvdvnkknqkhkmf025gq6rx4eret2xzeatlfajkeq7u2fxl55drpd96xeaxzns85sfxah9j',
-          lovelaces : 5000000, // 5 ADA = 5million lovelace
-        }}/>
-        
-      </td></tr></table>
+      {!wallets.length ? 'You have no Cardano wallets installed :-(' :
+        <table><tbody><tr><td>
+          <img src={logo} alt={addr} className='Qr'/>
+        </td></tr><tr><td>
+          Give me: <input
+            type       = 'number'
+            className  = 'Ada'
+            onChange   = {ada => setAda(ada.target.value)}
+            value      = {ada}
+            min        = {1}
+          /> ADA
+        </td></tr><tr><td>
+          
+          <PurchaseButton
+            id          = {addr}
+            label       = {'Send using'}
+            walletList  = {setWallets}
+            callback    = {setTxHash}
+            onError     = {setError}
+            txInfo      = {{
+              lovelaces: `${1000000*ada}`,
+              toAddress: `${addr}`,
+            }}
+          />
+          
+        </td></tr></tbody></table>
+      }
       {JSON.stringify(txHash)}
       {JSON.stringify(error)}
     </div>
--- src/component/PurchaseButton.js
+++ src/component/PurchaseButton.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import {
-  Address,
-  BigNum,
-  CoinSelectionStrategyCIP2,
-  LinearFee,
-  Transaction,
-  TransactionBuilder,
-  TransactionBuilderConfigBuilder,
-  TransactionOutput,
-  TransactionUnspentOutput,
-  TransactionUnspentOutputs,
-  TransactionWitnessSet,
-  Value,
-} from '@emurgo/cardano-serialization-lib-asmjs';
 
-function PurchaseButton({label='Purchase using', txInfo, callback, onError}) {
+function PurchaseButton({id, label='Purchase using', walletList, txInfo, callback, onError}) {
   // Protocol Parameters
   const pp = {
     min_fee_a           : 44,
     min_fee_b           : 155381,
-    coins_per_utxo_word : '34482',
-    coins_per_utxo_size : '34482',
+    coins_per_utxo_word : '4310',
+    coins_per_utxo_size : '4310',
     pool_deposit        : '500000000',
-    key_deposit         : '400000',
+    key_deposit         : '2000000',
     max_val_size        : '5000',
     max_tx_size         : 16384,
   };
@@ -38,80 +24,99 @@ function PurchaseButton({label='Purchase using', txInfo, callback, onError}) {
       }
     }
     setWallets(wallets.sort());
+    try {
+      walletList(wallets.sort());
+    }
+    catch {}
   }, []);
   
   return (
-    <table>
+    <table><tbody>
       {wallets.map(key =>
-        <tr><td><button className = 'Purchase-button'
-                        onClick   = {async () => {
+        <tr key={`${id}.${key}`}><td><button className='Purchase-button'
+          onClick={async () => {
             try {
-              const userWallet    = await window.cardano[key].enable();
-              const hexAddress    = await userWallet.getChangeAddress();
-              const wallet        = {
+              const userWallet  = await window.cardano[key].enable();
+              const hexAddress  = await userWallet.getChangeAddress();
+              const wallet      = {
                 api     : userWallet,
                 address : hexAddress,
               };
               
-              const minFeeA = BigNum.from_str('' + pp.min_fee_a);
-              const minFeeB = BigNum.from_str('' + pp.min_fee_b);
-              const feeAlgo = LinearFee.new(minFeeA, minFeeB);
-              
-              const coinsPerUtxoWord = BigNum.from_str(pp.coins_per_utxo_word);
-              // const coinsPerUtxoByte = BigNum.from_str(pp.coins_per_utxo_size);
-              
-              const poolDeposit = BigNum.from_str(pp.pool_deposit);
-              const keyDeposit  = BigNum.from_str(pp.key_deposit);
-              
-              const config = TransactionBuilderConfigBuilder.new()
-                .fee_algo(feeAlgo)
-                .coins_per_utxo_word(coinsPerUtxoWord)
-                // .coins_per_utxo_byte(coinsPerUtxoByte)
-                .pool_deposit(poolDeposit)
-                .key_deposit(keyDeposit)
-                .max_value_size(pp.max_val_size)
-                .max_tx_size(pp.max_tx_size)
-                .build();
-              const builder = TransactionBuilder.new(config);
-              
-              const numOutput = BigNum.from_str('' + txInfo.lovelaces);
-              const valOutput = Value.new(numOutput);
-              const to = Address.from_bech32(txInfo.toAddress);
-              const transactionOutput = TransactionOutput.new(to, valOutput);
-              builder.add_output(transactionOutput);
-              
-              const utxoS = await wallet.api.getUtxos();
-              const transactionUnspentOutputs = TransactionUnspentOutputs.new();
-              for(const utxo of utxoS) {
-                const transactionUnspentOutput = TransactionUnspentOutput.from_hex(utxo);
-                if(transactionUnspentOutput.output().to_js_value().amount.multiasset == null)
-                  transactionUnspentOutputs.add(transactionUnspentOutput);
-              }
-              builder.add_inputs_from(transactionUnspentOutputs,
-                CoinSelectionStrategyCIP2.RandomImprove);
-              
-              const changeAddress = Address.from_hex(wallet.address);
-              builder.add_change_if_needed(changeAddress);
-              
-              const txBody = builder.build();
-              const noWitness = TransactionWitnessSet.new();
-              const txNoWitness = Transaction.new(txBody, noWitness);
-              
-              const txRaw = txNoWitness.to_hex();
-              const txWitness = await wallet.api.signTx(txRaw);
-              
-              const witnessed = TransactionWitnessSet.from_hex(txWitness);
-              const txWitnessed = Transaction.new(txBody, witnessed);
-              
-              const txSigned = txWitnessed.to_hex();
-              const txHash = await wallet.api.submitTx(txSigned);
-              try {
-                callback({txHash});
-              }
-              catch {
-                const e = JSON.stringify({txHash});
-                console.log(e);
-              }
+              await import('@emurgo/cardano-serialization-lib-asmjs').then(async ({
+                Address,
+                BigNum,
+                CoinSelectionStrategyCIP2,
+                LinearFee,
+                Transaction,
+                TransactionBuilder,
+                TransactionBuilderConfigBuilder,
+                TransactionOutput,
+                TransactionUnspentOutput,
+                TransactionUnspentOutputs,
+                TransactionWitnessSet,
+                Value,
+              }) => {
+                const minFeeA = BigNum.from_str('' + pp.min_fee_a);
+                const minFeeB = BigNum.from_str('' + pp.min_fee_b);
+                const feeAlgo = LinearFee.new(minFeeA, minFeeB);
+                
+                const coinsPerUtxoWord = BigNum.from_str(pp.coins_per_utxo_word);
+                // const coinsPerUtxoByte = BigNum.from_str(pp.coins_per_utxo_size);
+                
+                const poolDeposit = BigNum.from_str(pp.pool_deposit);
+                const keyDeposit  = BigNum.from_str(pp.key_deposit);
+                
+                const config = TransactionBuilderConfigBuilder.new()
+                  .fee_algo(feeAlgo)
+                  .coins_per_utxo_word(coinsPerUtxoWord)
+                  // .coins_per_utxo_byte(coinsPerUtxoByte)
+                  .pool_deposit(poolDeposit)
+                  .key_deposit(keyDeposit)
+                  .max_value_size(pp.max_val_size)
+                  .max_tx_size(pp.max_tx_size)
+                  .build();
+                const builder = TransactionBuilder.new(config);
+                
+                const numOutput = BigNum.from_str('' + txInfo.lovelaces);
+                const valOutput = Value.new(numOutput);
+                const to = Address.from_bech32(txInfo.toAddress);
+                const transactionOutput = TransactionOutput.new(to, valOutput);
+                builder.add_output(transactionOutput);
+                
+                const utxoS = await wallet.api.getUtxos();
+                const transactionUnspentOutputs = TransactionUnspentOutputs.new();
+                for(const utxo of utxoS) {
+                  const transactionUnspentOutput = TransactionUnspentOutput.from_hex(utxo);
+                  if(transactionUnspentOutput.output().to_js_value().amount.multiasset == null)
+                    transactionUnspentOutputs.add(transactionUnspentOutput);
+                }
+                builder.add_inputs_from(transactionUnspentOutputs,
+                  CoinSelectionStrategyCIP2.RandomImprove);
+                
+                const changeAddress = Address.from_hex(wallet.address);
+                builder.add_change_if_needed(changeAddress);
+                
+                const txBody = builder.build();
+                const noWitness = TransactionWitnessSet.new();
+                const txNoWitness = Transaction.new(txBody, noWitness);
+                
+                const txRaw = txNoWitness.to_hex();
+                const txWitness = await wallet.api.signTx(txRaw);
+                
+                const witnessed = TransactionWitnessSet.from_hex(txWitness);
+                const txWitnessed = Transaction.new(txBody, witnessed);
+                
+                const txSigned = txWitnessed.to_hex();
+                const txHash = await wallet.api.submitTx(txSigned);
+                try {
+                  callback({txHash});
+                }
+                catch {
+                  const e = JSON.stringify({txHash});
+                  console.log(e);
+                }
+              });
             }
             catch(x) {
               try {
@@ -123,15 +128,15 @@ function PurchaseButton({label='Purchase using', txInfo, callback, onError}) {
               }
             }
           }}>
-            <table><tr><td>
+            <table><tbody><tr><td>
               <img src = {window.cardano[key].icon}
                   width={32} height={32} alt={key}/>
               </td><td>
               {`${label} ${window.cardano[key].name}`}
-            </td></tr></table>
+            </td></tr></tbody></table>
         </button></td></tr>
       )}
-    </table>
+    </tbody></table>
   );
 }
 
